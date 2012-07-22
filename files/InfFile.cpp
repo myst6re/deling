ac2d6b6c53526a5e07d6f229d6a1db10c0cf1cff
myst6re <myst6re@gmail.com>
2012-07-22 16:39:06 +0000

    Ooops, I forgot two files
    git-svn-id: https://svn.code.sf.net/p/deling/code/trunk@63 d07ef2b7-bbf6-4ce4-b825-8d9644e34689

---
+++ files/InfFile.cpp
@@ -0,0 +1,85 @@
+/****************************************************************************
+ ** Deling Final Fantasy VIII Field Editor
+ ** Copyright (C) 2009-2012 Arzel Jérôme <myst6re@gmail.com>
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#include "files/InfFile.h"
+
+InfFile::InfFile()
+	: modified(false)
+{
+}
+
+bool InfFile::open(const QByteArray &inf)
+{
+    if(sizeof(InfStruct) != inf.size()) {
+        qWarning() << "invalid inf size" << sizeof(InfStruct) << inf.size();
+        return false;
+    }
+
+	const char *constInf = inf.constData();
+
+    memcpy(&infStruct, constInf, sizeof(InfStruct));
+
+	return true;
+}
+
+bool InfFile::save(QByteArray &inf)
+{
+    inf.append((char *)&infStruct, sizeof(InfStruct));
+
+	modified = false;
+
+	return true;
+}
+
+bool InfFile::isModified() const
+{
+	return modified;
+}
+
+QString InfFile::getMapName() const
+{
+    return QString(QByteArray(infStruct.name, 8));
+}
+
+void InfFile::setMapName(const QString &mapName)
+{
+    char *name = mapName.toLatin1().leftJustified(8, '\0', true).data();
+
+    memcpy(&infStruct.name, name, 8);
+
+	modified = true;
+}
+
+QList<Gateway> InfFile::getGateways() const
+{
+    QList<Gateway> gates;
+
+    for(int i=0 ; i<12 ; ++i) {
+        gates.append(infStruct.gateways[i]);
+    }
+
+    return gates;
+}
+
+void InfFile::setGateways(const QList<Gateway> &gateways)
+{
+    for(int i=0 ; i<12 ; ++i) {
+        infStruct.gateways[i] = gateways.at(i);
+    }
+
+	modified = true;
+}
---
+++ files/InfFile.h
@@ -0,0 +1,58 @@
+/****************************************************************************
+ ** Deling Final Fantasy VIII Field Editor
+ ** Copyright (C) 2009-2012 Arzel Jérôme <myst6re@gmail.com>
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#ifndef INFFILE_H
+#define INFFILE_H
+
+#include <QtCore>
+#include "WalkmeshFile.h"
+
+typedef struct {
+    Vertex_s exitLine[2];
+    Vertex_s destinationPoint;
+    quint16 fieldId;
+    quint8 unknown[12];
+} Gateway;
+
+typedef struct {
+    quint8 unknown[16];
+} UnknownStruct;
+
+typedef struct {
+    char name[8];
+    quint8 unknown1[92];
+    Gateway gateways[12];
+    UnknownStruct unknown2[12];
+} InfStruct;
+
+class InfFile
+{
+public:
+    InfFile();
+    bool open(const QByteArray &inf);
+    bool save(QByteArray &inf);
+	bool isModified() const;
+    QString getMapName() const;
+	void setMapName(const QString &mapName);
+    QList<Gateway> getGateways() const;
+    void setGateways(const QList<Gateway> &gateways);
+private:
+    InfStruct infStruct;
+	bool modified;
+};
+
+#endif // INFFILE_H
